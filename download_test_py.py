36d7623723ece3768859e8899e055c9dfda323fc
kashifnez <58875866+kashifnez@users.noreply.github.com>
2020-10-01 19:01:33 +0530

    Update download_test_py.py
    Adding a Default path if user didn't enter anything in the saving folder

--- download_test_py.py
+++ download_test_py.py
@@ -13,8 +13,10 @@ def downloadVid():
     n=int(input("Enter your choice"))
     vid=videos[n-1]
     destination=str(input("Enter your destination"))
+    if destination == "":
+        destination = /sdcard/YouTubeVideoDownloads
     vid.download(destination)
-    print(yt.filename+"\n Ha been downloaded")
+    print(yt.filename+"\n Has been downloaded")
 root=tk.Tk()
 
 w=tk.Label(root,text="Youtube Downloader")
